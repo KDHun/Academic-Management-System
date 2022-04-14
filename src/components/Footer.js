31feb5bfe8700b7f9b4308ad3c9b467135e6b60f
Karabhai Hun <hun.karabhai@gmail.com>
2022-04-14 22:35:14 +0530

    first commite

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Footer = () => {
+var style = {
+    backgroundColor: "black",
+    borderTop: "1px solid 000000",
+    textAlign: "center",
+    padding: "10px",
+    position: "fixed",
+    left: "0",
+    bottom: "0",
+    height: "50px",
+    width: "100%",
+    color : "white",
+    
+};
+  return (
+    <div style={style}>
+        All Right Reserved @E-Study - 2022
+    </div>
+  )
+}
